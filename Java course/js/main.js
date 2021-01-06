fd962b24d3d2f812db58d94b14311d7369e0e9f9
Alex <weetp@mail.ru>
2021-01-06 14:42:06 +0300

    Это наш 1 комит

---
+++ Java course/js/main.js
@@ -0,0 +1,108 @@
+'use strict';
+a = 5;
+console.log(a)
+
+
+// let arr = ['plum.png', 'orange.png', 'apple.bmp'];
+
+// console.log(arr[2])
+
+
+// const obj = {
+//     name: 'alex'
+// }
+
+// const arr = [1, 2, 3, 4, 5, 6];
+
+// // console.log(typeof(obj))
+
+// console.log(typeof("www" + 5 + "ooo"))
+
+
+// console.log('https://vk.com/catalog/' + 5);
+
+
+
+// console.log(typeof(Number('5')));
+
+
+// console.log(typeof(+'5'))
+
+
+
+// console.log(typeof(parseInt('15px', 10)));
+
+
+
+// let ans = +prompt('hello', '');
+
+
+
+// let switcher = null;
+
+// if (switcher) {
+//     console.log("Work..")
+// }
+
+// switcher =  1;
+
+// if (switcher) {
+//     console.log("Work..")
+// }
+
+
+
+// console.log(typeof(Boolean('5')));
+
+
+
+// console.log(typeof(!!'5'));
+
+
+// let ans = confirm("Are u idiot?");
+
+// console.log(ans)
+
+// let arr = ['plum.png', 'orange.png', 'apple.bmp'];
+
+// let ans = prompt("Es li 19 ?", "", "да");
+
+// console.log(typeof(arr));
+
+
+
+let incr = 10,
+    decr = 10;
+
+
+
+console.log(incr++);
+console.log(decr--);
+
+console.log(5%2);
+
+
+console.log("2" === 2);
+
+
+let isChecked = false,
+    isClose = false;
+
+console.log(isChecked || !isClose);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
